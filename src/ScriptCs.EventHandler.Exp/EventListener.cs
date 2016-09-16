af1b41cddc2233ed805ccd2e9cab94f823d5be00
Amadeusz Wieczorek <git@amadeusw.com>
2016-09-15 22:17:54 -0700

    subscribes a snippet code into a system event

---
+++ src/ScriptCs.EventHandler.Exp/EventListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+
+namespace ScriptCs.EventManager.Exp
+{
+    public class EventListener
+    {
+        public void Subscribe(MySystemEvents powerModeChanged, Func<object, EventArgs, Task> handler)
+        {
+            switch (powerModeChanged)
+            { 
+                case MySystemEvents.PowerModeChanged:
+                    Microsoft.Win32.SystemEvents.PowerModeChanged += (s, e) => handler(s, e);
+                    // TODO: remember to unsubscribe
+                    return;
+                default:
+                    return;
+            }
+        }
+    }
+}
---
+++ src/ScriptCs.EventHandler.Exp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScriptCs.EventManager.Exp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var em = new EventListener();
+            em.Subscribe(MySystemEvents.PowerModeChanged, (s, ea) =>
+            {
+                var e = ea as Microsoft.Win32.PowerModeChangedEventArgs;
+                if (e.Mode == PowerModes.Resume)
+                {
+                    Console.WriteLine("Resumed! Haha.");
+                    return Task.FromResult(true);
+                }
+                else
+                {
+                    Console.WriteLine(e.Mode.ToString());
+                    return Task.FromResult(false);
+                }
+            });
+
+            Console.WriteLine("Let's see what happens when you put the computer to sleep!");
+            Console.ReadLine();
+        }
+    }
+}
